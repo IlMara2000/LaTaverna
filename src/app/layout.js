d025979563f939d4577f3ec8b826b507b8fca293
den <40234529+IlMara2000@users.noreply.github.com>
2026-03-11 21:41:59 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,1 +1,9 @@
+import "../style.css"; 
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="it">
+      <body className="bg-[#0a0a0a] text-white">{children}</body>
+    </html>
+  );
+}
