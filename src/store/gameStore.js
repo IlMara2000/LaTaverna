268a577577c4855603af7e05fc1e535441b5781d
den <40234529+IlMara2000@users.noreply.github.com>
2026-03-11 21:52:18 +0100

    Update gameStore.js

--- src/store/gameStore.js
+++ src/store/gameStore.js
@@ -1,11 +1,10 @@
 import { create } from 'zustand';
 
 export const useGameStore = create((set) => ({
-    currentSession: null,
     tokens: [],
-    setSession: (session) => set({ currentSession: session }),
     setTokens: (tokens) => set({ tokens }),
     updateToken: (tokenId, data) => set((state) => ({
         tokens: state.tokens.map(t => t.$id === tokenId ? { ...t, ...data } : t)
     })),
+    addToken: (token) => set((state) => ({ tokens: [...state.tokens, token] })),
 }));
