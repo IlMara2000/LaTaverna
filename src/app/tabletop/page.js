a2e11901bbb50631f7ec15735ab2c9ee3dc157ae
den <40234529+IlMara2000@users.noreply.github.com>
2026-03-11 21:42:30 +0100

    Update page.js

--- src/app/tabletop/page.js
+++ src/app/tabletop/page.js
@@ -1,1 +1,6 @@
-
+"use client";
+import React, { useEffect } from 'react';
+import MapGrid from '../../ui/components/vtt/MapGrid';
+import AIChatPanel from '../../ui/components/aichatpanel';
+import DiceRoller from '../../ui/components/vtt/DiceRoller';
+// ... resto del codice mixato ...
