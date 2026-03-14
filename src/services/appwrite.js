421f986a372f3ac21ac83bac1b9a10a2916ac3be
den <40234529+IlMara2000@users.noreply.github.com>
2026-03-14 22:29:02 +0100

    Update appwrite.js

--- src/services/appwrite.js
+++ src/services/appwrite.js
@@ -2,11 +2,10 @@ import { Client, Account, Databases, Storage } from 'appwrite';
 
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('66ed6258003666f5492d'); // Assicurati che questo ID sia corretto
+    .setProject('66ed6258003666f5492d');
 
 export const account = new Account(client);
 export const databases = new Databases(client);
-// QUESTO ERA IL PEZZO MANCANTE CHE BLOCCAVA VERCEL:
-export const storage = new Storage(client); 
+export const storage = new Storage(client); // Esportazione necessaria per il build
 
 export { client };
