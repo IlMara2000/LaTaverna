15a3348fb7fe309860d5bd8a0e33821eab20f873
den <40234529+IlMara2000@users.noreply.github.com>
2026-03-14 15:49:09 +0100

    Update appwrite.js

--- src/services/appwrite.js
+++ src/services/appwrite.js
@@ -3,9 +3,8 @@ import { Client, Account, Databases } from "appwrite";
 
 // Inizializziamo il client direttamente con i tuoi dati
 const client = new Client()
-  .setEndpoint('https://cloud.appwrite.io/v1') // L'endpoint standard di Appwrite Cloud
-  .setProject('IL_TUO_PROJECT_ID')           // <--- INCOLLA QUI IL TUO PROJECT ID DI APPWRITE
-  .setSelfSigned(true); 
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT);
 
 export const account = new Account(client);
 export const databases = new Databases(client);
