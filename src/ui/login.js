c7de7dda0db735097b8e66d3ad3210194ab0d223
den <40234529+IlMara2000@users.noreply.github.com>
2026-03-15 12:10:24 +0100

    Update login.js

--- src/ui/login.js
+++ src/ui/login.js
@@ -1,16 +1,17 @@
-import { account } from '@services/appwrite.js'
-import { showDashboard } from '@ui/dashboard.js'
-import { showRegister } from '@ui/register.js'
+import { account } from "../services/appwrite.js"
+
+import { showDashboard } from "./dashboard.js"
+import { showRegister } from "./register.js"
 
 export function showLogin(container){
 
-document.title="LaTaverna - Login"
+document.title="Login - La Taverna"
 
 container.innerHTML=`
 
 <div class="glass-box">
 
-<h2>ACCEDI</h2>
+<h2>Accedi alla Taverna</h2>
 
 <div id="login-msg"></div>
 
@@ -20,29 +21,41 @@ container.innerHTML=`
 
 <input type="password" id="password" placeholder="Password" required>
 
-<button class="btn">Accedi</button>
+<button class="btn">
+Entra nella Taverna
+</button>
 
 </form>
 
-<div class="divider"><span>oppure</span></div>
-
-<button id="btn-register" class="btn-alt">Registrati</button>
-
+<div class="divider">
+<span>oppure</span>
 </div>
 
+<button id="discord-login" class="btn-alt">
+Entra con Discord
+</button>
+
+<button id="btn-register" class="btn-alt">
+Crea Personaggio
+</button>
+
+</div>
 `
 
-const form=document.getElementById('login-form')
+const msg=document.getElementById("login-msg")
 
-form.addEventListener('submit',async e=>{
+document.getElementById("login-form")
+.onsubmit=async e=>{
 
 e.preventDefault()
 
 try{
 
 await account.createEmailPasswordSession(
+
 email.value,
 password.value
+
 )
 
 const user=await account.get()
@@ -51,13 +64,26 @@ showDashboard(container,user)
 
 }catch(err){
 
-document.getElementById('login-msg').innerText=err.message
+msg.textContent=err.message
 
 }
 
-})
+}
 
-document.getElementById('btn-register')
+document.getElementById("discord-login")
+.onclick=async()=>{
+
+await account.createOAuth2Session(
+
+"discord",
+window.location.origin,
+window.location.origin
+
+)
+
+}
+
+document.getElementById("btn-register")
 .onclick=()=>showRegister(container)
 
 }
