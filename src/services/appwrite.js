ac5dbd50483910d279b187364330074ffc151774
den <40234529+IlMara2000@users.noreply.github.com>
2026-03-15 13:45:39 +0100

    Update appwrite.js

--- src/services/appwrite.js
+++ src/services/appwrite.js
@@ -2,7 +2,7 @@ import { Client, Account, Databases, Storage } from 'appwrite';
 
 const client = new Client()
     .setEndpoint('https://nyc.cloud.appwrite.io/v1')
-    .setProject('66ed6258003666f5492d');
+    .setProject('66ed6258003666f5492d'); // <-- CAMBIA QUESTO
 
 export const account = new Account(client);
 export const databases = new Databases(client);
