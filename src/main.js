4eca48ec4478d6f6fa77b3dce3f29924b1cdf262
den <40234529+IlMara2000@users.noreply.github.com>
2026-03-17 14:00:34 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,30 @@
 import './style.css'
 import { showLogin } from './ui/login.js'
 import { account } from './services/appwrite.js'
+import { showDashboard } from './ui/dashboard.js'
+import { setupDiscordRedirect } from './services/redirectDiscord.js'
 
 const ui = document.getElementById('ui');
 
 async function init() {
-    ui.innerHTML = `
-        <div class="landing-no-box" id="portal" style="cursor:pointer;">
-            <img src="/assets/logo.png" style="width:220px; filter: drop-shadow(0 0 15px var(--neon-glow));">
-            <h2 class="auth-title" style="font-size:1rem; letter-spacing:2px; animation: pulse 2s infinite;">
-                Tocca la Coppa per Entrare
-            </h2>
-        </div>
-    `;
+    // Gestione redirect Discord
+    await setupDiscordRedirect(ui);
 
-    document.getElementById('portal').onclick = () => showLogin(ui);
+    try {
+        const user = await account.get();
+        if (user) {
+            showDashboard(ui, user);
+        }
+    } catch {
+        ui.innerHTML = `
+            <div class="landing-container" id="portal" style="cursor:pointer;">
+                <img src="/assets/logo.png" class="landing-logo">
+                <h2 class="landing-text" style="animation: pulse 2s infinite;">
+                    Tocca la Coppa per Entrare
+                </h2>
+            </div>
+        `;
+        document.getElementById('portal').onclick = () => showLogin(ui);
+    }
 }
 init();
